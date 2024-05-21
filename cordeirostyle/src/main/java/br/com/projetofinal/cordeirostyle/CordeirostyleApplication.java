59979e99006d027db82914b9556b905df7427b59
LucasBoeck23 <162763345+LucasBoeck23@users.noreply.github.com>
2024-05-20 21:59:06 -0300

    build

---
+++ cordeirostyle/src/main/java/br/com/projetofinal/cordeirostyle/CordeirostyleApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetofinal.cordeirostyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordeirostyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordeirostyleApplication.class, args);
+	}
+
+}
---
+++ cordeirostyle/src/test/java/br/com/projetofinal/cordeirostyle/CordeirostyleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetofinal.cordeirostyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CordeirostyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
