a73cc99c4de421c35442337839315c3c2eb03158
Khezac <khezac1@gmail.com>
2024-05-24 21:36:29 -0300

    fix: retornos dos DTOs nos métodos crud

--- cordeirostyle/src/main/java/br/com/projetofinal/cordeirostyle/services/PedidoService.java
+++ cordeirostyle/src/main/java/br/com/projetofinal/cordeirostyle/services/PedidoService.java
@@ -8,7 +8,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.projetofinal.cordeirostyle.dtos.ItemPedidoDto;
 import br.com.projetofinal.cordeirostyle.dtos.PedidoDto;
+import br.com.projetofinal.cordeirostyle.dtos.ProdutoDtoRetorno;
+import br.com.projetofinal.cordeirostyle.entities.ItemPedido;
 import br.com.projetofinal.cordeirostyle.entities.Pedido;
 import br.com.projetofinal.cordeirostyle.repositories.PedidoRepository;
 
@@ -28,9 +31,20 @@ public class PedidoService {
 	        throw new NoSuchElementException("Não há pedidos registrados!");
 	    }
 
-	    for (Pedido pedido : pedidos) { 
+	    for(Pedido pedido : pedidos) { 
 	        PedidoDto pedidoTransformado = modelMapper.map(pedido, PedidoDto.class);
+	        List<ItemPedido> itensPedidos = pedido.getItensPedidos();
+	        List<ItemPedidoDto> itensDto = new ArrayList<>();
 
+	        for(ItemPedido item : itensPedidos) {
+	        	ItemPedidoDto itemPedidoDto = modelMapper.map(item, ItemPedidoDto.class);
+	        	ProdutoDtoRetorno produtoDtoRetorno = modelMapper.map(item.getProduto(), ProdutoDtoRetorno.class);
+	        	itemPedidoDto.setProdutoDto(produtoDtoRetorno);
+	        	
+				itensDto.add(itemPedidoDto);
+			}
+	        
+	        pedidoTransformado.setItens(itensDto);
 	        pedidoDtoList.add(pedidoTransformado);
 	    }
 	    return pedidoDtoList;
@@ -38,8 +52,22 @@ public class PedidoService {
 
 
 	public PedidoDto findById(Integer id) {
-		Pedido pedido = pedidoRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Categoria não encontrada!"));
-		PedidoDto pedidoDto = modelMapper.map(pedido, PedidoDto.class);
+		Pedido pedido = pedidoRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Pedido não encontrado!"));
+		PedidoDto pedidoDto = null;
+		
+		List<ItemPedido> itensPedidos = pedido.getItensPedidos();
+		List<ItemPedidoDto> itensDto = new ArrayList<>();
+				
+		for(ItemPedido item : itensPedidos) {
+        	ItemPedidoDto itemPedidoDto = modelMapper.map(item, ItemPedidoDto.class);
+        	ProdutoDtoRetorno produtoDtoRetorno = modelMapper.map(item.getProduto(), ProdutoDtoRetorno.class);
+        	itemPedidoDto.setProdutoDto(produtoDtoRetorno);
+        	
+			itensDto.add(itemPedidoDto);
+		}
+		
+		pedidoDto = modelMapper.map(pedido, PedidoDto.class);
+		pedidoDto.setItens(itensDto);
 		
 		return pedidoDto;
 	}
@@ -48,7 +76,6 @@ public class PedidoService {
 	    Pedido pedido = modelMapper.map(pedidoDto, Pedido.class);
 	    Pedido pedidoSalvo = pedidoRepository.save(pedido);
 	    PedidoDto pedidoSalvoDto = modelMapper.map(pedidoSalvo, PedidoDto.class);
-	 
 	    return pedidoSalvoDto;
 	}
 
@@ -56,16 +83,12 @@ public class PedidoService {
 		Pedido pedidoAtualizado = pedidoRepository.findById(id).orElse(null);
 		PedidoDto pedidoDtoAtualizado = null;
 		if (pedidoAtualizado != null) {
-			try {
 				pedidoAtualizado.setData_entrega(pedido.getData_entrega());
 				pedidoAtualizado.setData_envio(pedido.getData_envio());
 				pedidoAtualizado.setStatus(pedido.getStatus());
 				pedidoAtualizado.setValor_total(pedido.getValor_total());
 				pedidoDtoAtualizado = modelMapper.map(pedidoAtualizado, PedidoDto.class);
 				pedidoRepository.save(pedidoAtualizado);
-			} catch (Exception e) {
-				System.out.println(e);
-			}
 		}
 		return pedidoDtoAtualizado;
 	}
